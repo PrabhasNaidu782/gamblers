7f74048400f60a0875bb17c6da67da200defdcf3
PrabhasNaidu782 <2475969@cognizant.com>
2026-04-08 11:31:07 +0530

    card updates

--- obj/Debug/net8.0/GamblersGrocery.AssemblyInfo.cs
+++ obj/Debug/net8.0/GamblersGrocery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamblersGrocery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d30c4da710a435668a8fe5b9b6062117306047")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d9dada3ffb70bde2319a01e7f4db3cf3080dbfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamblersGrocery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamblersGrocery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
