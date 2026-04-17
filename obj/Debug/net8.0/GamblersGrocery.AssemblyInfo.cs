2172d6f8b2492809ea7c86caab34021360315820
2475889 <2475889@LTIN691721>
2026-04-17 14:35:45 +0530

    Api Added

--- obj/Debug/net8.0/GamblersGrocery.AssemblyInfo.cs
+++ obj/Debug/net8.0/GamblersGrocery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamblersGrocery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1ec5bcfa38a84285efa0cecfe736663a85acb81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efb74e2839f5ed7f42d95dbd2ec087c37d79e14a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamblersGrocery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamblersGrocery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
